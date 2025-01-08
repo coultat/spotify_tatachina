0148ae6c152575f869e3f11463c29a2fed04de93
Felipe Míguez Kautzmann <fkautzmann@immomio.de>
2025-01-08 18:18:13 +0100

    fix: add model

--- spotify/models.py
+++ spotify/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+    url = models.URLField(max_length=500)
+    height = models.IntegerField()
+    width = models.IntegerField()
+
+    def __str__(self):
+        return self.url
+
+
+class Artist(models.Model):
+    spotify_url = models.URLField(max_length=500)
+    followers_total = models.IntegerField()
+    genres = models.JSONField()
+    api_href = models.CharField(max_length=500)
+    spotify_id = models.Charfield(max_length=50)
+    name = models.CharField(max_length=200)
+    popularity = models.IntegerField()
+    uri = models.CharField(max_length=100)
+    images = models.ManyToManyField(Image)
+
+    def __str__(self):
+        return self.name
+
