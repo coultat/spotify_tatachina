43020b20181dc12fbde2fec6650b210073f343c1
Felipe Míguez Kautzmann <fkautzmann@immomio.de>
2025-01-13 09:40:13 +0100

    feat: serializer added

--- spotify/serializers.py
+++ spotify/serializers.py
@@ -1,8 +1,27 @@
 from rest_framework import serializers
-# from spotify.models import Artist
-#
-#
-# class ArtistSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model=Artist
-#         fields="__all__"
+from .models import Artist, Image
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = ['url', 'height', 'width']
+
+class ArtistSerializer(serializers.ModelSerializer):
+    images = ImageSerializer(many=True)
+
+    class Meta:
+        model = Artist
+        fields = [
+            'spotify_url', 'followers_total', 'genres', 'api_href',
+            'spotify_id', 'name', 'popularity', 'uri', 'images'
+        ]
+
+    def create(self, validated_data):
+        images_data = validated_data.pop('images')
+        artist = Artist.objects.create(**validated_data)
+
+        for image_data in images_data:
+            image = Image.objects.create(**image_data)
+            artist.images.add(image)
+
+        return artist
