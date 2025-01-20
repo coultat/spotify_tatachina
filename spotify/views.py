44136c0058af796ed5ec4ef5a7f56fa6be4077d7
Felipe Míguez Kautzmann <fkautzmann@immomio.de>
2025-01-20 19:10:21 +0100

    fix: ruffed

--- spotify/views.py
+++ spotify/views.py
@@ -21,7 +21,7 @@ def homepage() -> Response:
 @api_view(http_method_names=["GET"])
 def get_artist(request: Request) -> Response:
     artist_id = request.data["artist_id"]
-    db_artist_id = artist_id.split('?')[0]
+    db_artist_id = artist_id.split("?")[0]
     if result := Artist.objects.filter(spotify_id=db_artist_id).first():  # get / filter
         artist = ArtistSerializer(result)
         return Response(data=artist.data, status=status.HTTP_200_OK)
@@ -32,11 +32,16 @@ def get_artist(request: Request) -> Response:
         if artist.is_valid():
             artist.save()
             return Response(data={"result": result.json()}, status=status.HTTP_200_OK)
-        return Response(data={"error": f"Validation Error {artist.errors}"}, status=status.HTTP_409_CONFLICT)
+        return Response(
+            data={"error": f"Validation Error {artist.errors}"},
+            status=status.HTTP_409_CONFLICT,
+        )
     except TimeoutException as err:
         return Response(
             data={"error": f"Connection Timeout when requesting external data {err=}"},
             status=status.HTTP_503_SERVICE_UNAVAILABLE,
         )
     except HTTPStatusError as err:
-        return Response(data={"error": f"{err.args=}"}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(
+            data={"error": f"{err.args=}"}, status=status.HTTP_400_BAD_REQUEST
+        )
